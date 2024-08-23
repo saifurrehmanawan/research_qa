5fd54b02fd29cf993dbda4b2e13493b9cdf73d9b
Saif Ur Rehman <145745643+saifurrehmanawan@users.noreply.github.com>
2024-08-23 16:24:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,19 +12,25 @@ def main():
     # Add logo at the top
     #st.image("logo.png", width=100)
 
-    st.title("NCBC ASTRO LLAMA")
-    st.info("NCBC ASTRO LLAMA is an advanced chatbot designed to answer a wide range of astronomical questions efficiently based on YouTube podcasts, utilizing information from YouTube podcasts.")
+    st.title("Arxiv Q&A Assistant")
+    st.info("The Arxiv Q&A Assistant is an AI-driven tool designed to answer user questions by searching and extracting relevant information from research papers on arXiv. By leveraging advanced natural language processing techniques, this program allows users to interact with academic research more effectively, providing concise and accurate answers to their queries based on the latest scientific literature. Ideal for researchers, students, and academics, it simplifies the process of finding precise information within a vast array of research documents.")
 
     # User input for the question
     question = st.text_input("Ask a question about astronomy:")
 
     if st.button("Get Answer"):
+      st.write('Extracting keywords...')
       keywords_list = research_paper_qa.get_keywords(question)
+      st.write('Fetching arxiv papers...')
       df = research_paper_qa.fetch_arxiv_papers(keywords_list)
+      st.write('Creating vector database...')
       vector_database = research_paper_qa.ret_docs(df)
+      st.write('Retrieving relevant arxiv paper...')
       doc = research_paper_qa.retrieve(question, vector_database)
       title = research_paper_qa.title_extract(str(doc))
+      st.write('Downloading...')
       filename = research_paper_qa.download_arxiv_paper(title)
+      st.write('Answering...')
       research_paper_qa.rp_qa(question, filename, title)
       research_paper_qa.del_file(filename)
 
